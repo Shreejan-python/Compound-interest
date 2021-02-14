5bed42989af4ef9fb6c36a2cfa195e079d1f6c5c
Shreejan <dolaishreejan@gmail.com>
2021-02-14 14:27:48 +0530

    Initial commit

---
+++ compound.py
@@ -0,0 +1,24 @@
+print("How many years will you be saving ?")
+years = int(input("Enter years : "))
+
+print("How much money is currently in your account ? ")
+principal = float(input("Enter the current amount in your account : "))
+
+print("How much money do you plan on investing monthly ?")
+monthly_invest = float(input("Enter the amount : "))
+
+print("What do you estimate will be the yearly interest of this investment ?")
+interest = float(input('Enter the interest in decimal(10% = 0.1) : '))
+
+print("Calculating...\n\n\n\n\n\n\n\n\n\n")
+
+monthly_invest = monthly_invest * 12
+final_amount = 0
+
+for i in range(0, years):
+    if final_amount==0:
+        final_amount=principal
+
+    final_amount = (final_amount + monthly_invest) * (1  + interest)
+
+print(f"This is how much money you got have in your acount after {years} years", format(years) + str(final_amount))
